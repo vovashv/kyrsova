353a92b375a89b9a17a550bee2b19a57e577d344
prorok.rv <prorok.rv@gmail.com>
2019-06-02 21:06:53 +0300

    add EFContext

---
+++ WpfAppKyrsova/EFContext/EFContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppKyrsova.EFContext
+{
+    public class EFContext: DbContext
+    {
+        public EFContext():
+            base("DefoultConnection")
+        { }
+        public DbSet<Answers> Answer { get; set; }
+        public DbSet<Questions> Question { get; set; }
+
+    }
+}
