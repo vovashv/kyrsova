d25484507a1b2cf821296d28c6cf0b08e4496ea0
prorok.rv <prorok.rv@gmail.com>
2019-06-02 21:15:55 +0300

    Add connection to local Db

--- WpfAppKyrsova/EFContext/Questions.cs
+++ WpfAppKyrsova/EFContext/Questions.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WpfAppKyrsova.EFContext
 {
+        [Table("tblQuestions")]
     public class Questions
     {
+        [Key]
+        public int Id { get; set; }
+        public string Question { get; set; }
+
     }
 }
