59b4508961d7263c24df25e10cdefdf6faa0e59e
prorok.rv <prorok.rv@gmail.com>
2019-06-02 21:18:32 +0300

    add tbl Answers

--- WpfAppKyrsova/EFContext/Answers.cs
+++ WpfAppKyrsova/EFContext/Answers.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WpfAppKyrsova.EFContext
 {
+    [Table("tblAnswers")]
     public class Answers
     {
+        [Key]
+        public int Id { get; set; }
+        public string Answer { get; set; }
     }
 }
